94ca0608ed63fcc275e1591b5a9284090b5f889a
JosuedGarm52 <jaccobjosue@gmail.com>
2024-07-31 23:55:08 -0500

    first commit

---
+++ CapaPresentacionBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CapaPresentacionBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
